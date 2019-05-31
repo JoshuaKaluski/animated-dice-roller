12fcb71243311e0ab2d1256ab7951a6835f692cc
Joshua Kaluski <joshua@kaluski.com>
2019-05-31 12:06:52 -0400

    Improved button functionality: text changes and button becomes disabled when rolling.

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -10,8 +10,12 @@ class Rolldice extends Component {
     };
     constructor(props) {
         super(props);
-        //Initial state for each die
-        this.state = {die1: 'one', die2: 'one'};
+        this.state = {
+            //Initial face for each die
+            die1: 'one', die2: 'one',
+            //Boolean representing if the dice are being rolled
+            rolling: false
+        };
 
         //Bind this to method roll()
         this.roll = this.roll.bind(this);
@@ -20,14 +24,22 @@ class Rolldice extends Component {
         //Shorten syntax by assigning this.props.sides to const faces
         const faces = this.props.sides;
 
-        //Roll 2 dice randomly
-        for (let i = 0; i < 2; i++) {
-            //Generate a new roll by picking a random element from the array faces
-            let newRoll = faces[Math.floor(Math.random() * faces.length)];
+        //Setting rolling to true before rolling new dice as a callback function
+        this.setState({rolling: true}, () => {
+            //Roll 2 dice randomly
+            for (let i = 0; i < 2; i++) {
+                //Generate a new roll by picking a random element from the array faces
+                let newRoll = faces[Math.floor(Math.random() * faces.length)];
 
-            //Set state with new roll using a ternary expression to determine which die to set
-            (i === 1) ? this.setState({die2: newRoll}) : this.setState({die1: newRoll});
-        }
+                //Set state with new roll using a ternary expression to determine which die to set
+                (i === 1) ? this.setState({die2: newRoll}) : this.setState({die1: newRoll});
+            }
+        });
+
+        //Wait 1 second then set rolling to false
+        setTimeout(() => {
+            this.setState({rolling: false})
+        }, 1000)
     }
     render() {
         return (
@@ -37,8 +49,11 @@ class Rolldice extends Component {
                 <Die face={this.state.die1} />
                 <Die face={this.state.die2} />
             </div>
-            {/*Button to roll dice*/}
-            <button onClick={this.roll}>Roll Dice!</button>
+            {/*Button to roll dice; disabled when rolling*/}
+            <button onClick={this.roll} disabled={this.state.rolling}>
+                {/*Text changes depending on the value of rolling*/}
+                {(this.state.rolling) ? "Rolling..." : "Roll Dice!"}
+            </button>
         </div>
         );
     }
